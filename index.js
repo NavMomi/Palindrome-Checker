7024d89fe5f213dcd11397a71578c09f2a865a36
Navjot Kaur <99341365+NavMomi@users.noreply.github.com>
2022-07-25 10:26:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,23 +16,3 @@ function isPalindrome(str){
 //let x=isPalindrome("dad");
 let x=isPalindrome("Navjot");
 console.log(x);
-
-
-//
-function fact(num){
-    if(num===0){
-            return 1;
-        }else{
-            return num*fact(num-1);
-
-        }
-
-}
-        
-  
-
-  
-
-let z=fact(7);
-console.log(z);
-
